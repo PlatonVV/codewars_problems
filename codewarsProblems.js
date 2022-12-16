4ce5079ec27737786fec798259a9669e69792654
PlatonVV <platonvolodin@icloud.com>
2022-12-16 11:44:24 -0500

    Money Money Money

--- codewarsProblems.js
+++ codewarsProblems.js
@@ -1,3 +1,10 @@
 function minMax(arr){
     return [Math.min(...arr), Math.max(...arr)];
 }
+
+function calculateYears(principal, interest, tax, desired) {
+    let year=0;
+    while (principal<desired){year++; principal=principal+((principal*interest)-(principal*interest*tax))}
+    return year
+}
+
