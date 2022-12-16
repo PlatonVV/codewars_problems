b4b0d2724c204657b82f2dcf9fab7215295a531b
PlatonVV <platonvolodin@icloud.com>
2022-12-16 11:51:28 -0500

    Isograms

--- codewarsProblems.js
+++ codewarsProblems.js
@@ -15,3 +15,22 @@ function firstNonConsecutive (arr) {
 
     return (Number.isInteger(result)) ? result : null;
 }
+
+
+function isIsogram(str) {
+    if (str.isEmpty) {
+        return true;
+    } else {
+        str = str.toLowerCase();
+    }
+    let array = str.split('');
+    var sortedArr = array.slice().sort();
+
+    for (var i = 0; i < array.length; i++) {
+        if (sortedArr[i + 1] == sortedArr[i]) {
+            return false;
+        }
+    }
+    //else return true
+    return true;
+}
