ea8b4d3d2502988e6eb4f5f9604dd9374bc3e5b7
PlatonVV <platonvolodin@icloud.com>
2022-12-16 12:00:13 -0500

    fake binary

--- codewarsProblems.js
+++ codewarsProblems.js
@@ -28,3 +28,14 @@ function testEven(n) {
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
+function fakeBin(x){
+    let output = "";
+    for (let i=0; i<x.length; i+=1) {
+        if (x[i] < 5) {
+            output += 0;
+        } else { output += 1; }
+    }
+    return output;
+}
+
